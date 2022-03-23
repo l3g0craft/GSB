019458352b813386ca86e444a16a829ea39320a6
l3g0craft <acorre1@estran-brest.education>
2022-03-23 18:12:44 +0100

    Update testons.php

--- gf3/testons.php
+++ gf3/testons.php
@@ -3,18 +3,50 @@
         <title>Last 10 Results</title>
     </head>
     <body>
-        
-        <?php
-            include 'fonctiongenevisiteur.php';
-            // Connexion    la base de donn  es gsb_frais
-            $cnxBDD = connexion();
+        <h1>fiche de frais :
+    <table>
+        <tr>
+            <td>
+                date
+            </td>
+            <td>
+                supprimer
+            </td>
+            <td>
+                modifier
+            </td>
+            <td>
+                voir
+            </td>
+        </tr>
 
-            $idforfait = "SELECT id FROM etat" ;
-            $result= $cnxBDD->query($idforfait);
-            while ($row = mysqli_fetch_assoc($result)){
+    <?php
             echo "hello";
-            }
-        ?>
+
+        include 'fonctiongenevisiteur.php';
+
+        // Connexion    la base de donn  es gsb_frais
+        $cnxBDD = connexion();
+         echo "hello";
+
+        $montant= "SELECT id FROM fichefrais " ;
+        $result= $cnxBDD->query($montant);
+        while ($row = mysqli_fetch_assoc($result)){
+            ?>
+            
+
+                <tr>
+                <td>  
+                    <?php print($row['id']); ?>
+                </td>
+                <td>
+                    <h1>modifier</h1>
+                </td>
+                </tr>
+            </table>
+
+        <?php } ?>
+
             
     </body>
 </html>
