427321c3038e1c1cb8200e083f4850817d11dc2d
l3g0craft <acorre1@estran-brest.education>
2022-04-28 17:57:41 +0200

    Update fonctiongenevisiteur.php

--- fonctiongenevisiteur.php
+++ fonctiongenevisiteur.php
@@ -5,9 +5,9 @@ function connexion(){
             //local
             $host = "127.0.0.1";
             $user = "root";
-            $password = "leahpar29";
+            $password = "password";
             $dbname = "gsb_frais";
-            $port='3306';
+            $port='3307';
 
             
             //serveur
